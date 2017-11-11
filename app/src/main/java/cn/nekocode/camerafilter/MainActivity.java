d5df319fbc77324b99cab92b8e0e7f3be58dbc2a
Jongho <ljh30321@naver.com>
2017-11-11 23:51:27 +0900

    First commit
    checking textureview.getbitmap() method. It's good.

---
+++ app/src/main/java/cn/nekocode/camerafilter/MainActivity.java
@@ -0,0 +1,203 @@
+/*
+ * Copyright 2016 nekocode
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.nekocode.camerafilter;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * @author nekocode (nekocode.cn@gmail.com)
+ */
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CAMERA_PERMISSION = 101;
+    private CameraRenderer renderer;
+    private TextureView textureView;
+    private int filterId = R.id.filter0;
+    private Button captureButton;
+    private TextView tv_saved;
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Button captureButton = (Button) findViewById(R.id.capturebutton);
+        final TextView tv_saved = (TextView) findViewById(R.id.text_tv);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                Toast.makeText(this, "Camera access is required.", Toast.LENGTH_SHORT).show();
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                        REQUEST_CAMERA_PERMISSION);
+            }
+
+        } else {
+            setupCameraPreviewView();
+        }
+
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        capture();
+                        Bitmap bitmap = textureView.getBitmap();
+                        imageView.setImageBitmap(bitmap);
+                        Toast.makeText(getApplicationContext(),"capture", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA_PERMISSION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    setupCameraPreviewView();
+                }
+            }
+        }
+    }
+
+    void setupCameraPreviewView() {
+        renderer = new CameraRenderer(this);
+        textureView = (TextureView) findViewById(R.id.textureView);
+        assert textureView != null;
+        textureView.setSurfaceTextureListener(renderer);
+
+        // Show original frame when touch the view
+        textureView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+
+                    // 터치 시 오리지날 보여줌
+                    case MotionEvent.ACTION_DOWN:
+                        renderer.setSelectedFilter(R.id.filter0);
+                        break;
+
+                        // 터치를 떼면 다시 선택된 필터를 적용
+                    case MotionEvent.ACTION_CANCEL:
+                        renderer.setSelectedFilter(filterId);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        textureView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                renderer.onSurfaceTextureSizeChanged(null, v.getWidth(), v.getHeight());
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.filter, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        filterId = item.getItemId();
+
+        // TODO: need tidy up
+        if (filterId == R.id.capture) {
+
+            return true;
+        }
+
+
+        if (renderer != null)
+            renderer.setSelectedFilter(filterId);
+
+        return true;
+    }
+
+    private void capture() {
+        String filename;
+        Date date = new Date(0);
+        SimpleDateFormat sdf = new SimpleDateFormat ("yyyyMMddHHmmss");
+        filename =  sdf.format(date);
+
+        try{
+            String path = Environment.getExternalStorageDirectory().toString();
+            OutputStream fOut = null;
+            File file = new File(path, "/DCIM/Signatures/"+filename+".jpg");
+            fOut = new FileOutputStream(file);
+
+            Bitmap bitmap = textureView.getBitmap();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
+            fOut.flush();
+            fOut.close();
+
+            MediaStore.Images.Media.insertImage(getContentResolver()
+                    ,file.getAbsolutePath(),file.getName(),file.getName());
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
